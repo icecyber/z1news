fe73badbba452b9e9b97428c3cb4b2cf09fbb631
Heng Ratanak <icecyber99@gmail.com>
2023-01-30 10:05:05 +0700

    Add PWA and test it

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: false,
   swcMinify: true,
   images: {
     domains: ['wp4.z1flexiblesolution.com', 'localhost', 'facebook.com'],
   },
-};
+});
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
